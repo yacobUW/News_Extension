c06e735cd6dfa92b8a07ef72feae7019c84e8a7a
yacobUW <122832467+yacobUW@users.noreply.github.com>
2023-07-06 10:09:06 -0700

    Add files via upload

---
+++ SRC/Api_script.js
@@ -0,0 +1,16 @@
+var url = 'https://newsapi.org/v2/top-headlines?' +
+          'country=us&' +
+          'apiKey=a4a5d9b1473e48439c7f229bd917f18e';
+
+var req = new Request(url);
+
+fetch(req)
+  .then(function(response) {
+    //return response.json(); // Return the JSON Promise
+  })
+  .then(function(data) {
+    console.log(data); // Access the data in the resolved Promise
+  })
+  .catch(function(error) {
+    console.error('Error:', error);
+  });
